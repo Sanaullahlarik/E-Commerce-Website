3ddaf7d27914715b71c2e6725585a308610fad56
Sanaullah <sanaullahlarik.xwave@gmail.com>
2025-01-07 11:16:32 +0500

    I emplemented the error and set the path of signIn component for live the website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./Components/Layout/AppLayout";
 import SignUp from "./Components/auth/Registration/SignUp";
-import SignIn from "./Components/auth/LogIn/SignIn";
+import SignIn from './Components/auth/LogIn/SignIn';
 import PageNotFound from "./Components/page-not-found/pageNotFound";
 import ProductDetails from "./Components/product-details/ProductDetails";
 import { Provider } from "react-redux";
